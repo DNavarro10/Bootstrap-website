ff67f71c02f6783b23ace01a7087108999432ad2
DNavarro10 <luis_d_100@hotmail.com>
2019-03-25 00:15:01 -0600

    Completado

--- Js/scrollreveal.js
+++ Js/scrollreveal.js
@@ -1,5 +1,7 @@
-<script>
-window.sr = ScrollReveal();
+window.sr =ScrollReveal({duration:1000});
 
-sr.reaveal('.site-content .d-flex');
-</script>
+sr.reveal('.site-content .d-flex');
+sr.reveal('.section-1 .card');
+sr.reveal('.section-2 .d-flex');
+sr.reveal('.section-3 .col-md-4');
+sr.reveal('.section-4 .col-md-7, .col-md-5');
